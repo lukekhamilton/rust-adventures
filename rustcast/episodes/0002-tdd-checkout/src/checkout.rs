c6bccfd55803b71e59b5acd5d790ef00e4720fe9
Luke Hamilton <luke@mettadata.com>
2020-02-21 18:06:47 +1100

    Refactored to have tests in the context of the struct its testing.

--- rustcast/episodes/0002-tdd-checkout/src/checkout.rs
+++ rustcast/episodes/0002-tdd-checkout/src/checkout.rs
@@ -1,4 +1,5 @@
 use crate::item::Item;
+use crate::item::Item::*;
 use crate::price_list::PriceList;
 
 pub struct Checkout {
@@ -31,5 +32,52 @@ impl Checkout {
     pub fn scan(&mut self, item: Item) {
         println!("scanned item: {:?}", item);
         self.items.push(item);
+    }
+}
+
+mod tests {
+    use super::*;
+
+    fn assert_total(items: Vec<Item>, expected_total: f64) {
+        let mut checkout = build_check_out();
+        for &item in items.iter() {
+            checkout.scan(item);
+        }
+        let actual_total = checkout.total();
+        assert_eq!(actual_total, expected_total);
+    }
+
+    fn build_check_out() -> Checkout {
+        let mut price_list = PriceList::new();
+        price_list.set(Apple, 0.40);
+        price_list.set(Tomato, 0.25);
+        price_list.set(Mango, 1.80);
+
+        Checkout::new(price_list)
+    }
+
+    #[test]
+    fn test_checkout_empty() {
+        assert_total(vec![], 0.0);
+    }
+
+    #[test]
+    fn test_checkout_with_1_apple() {
+        assert_total(vec![Apple], 0.4);
+    }
+
+    #[test]
+    fn test_checkout_with_1_tomato() {
+        assert_total(vec![Tomato], 0.25);
+    }
+
+    #[test]
+    fn test_checkout_with_1_mango() {
+        assert_total(vec![Mango], 1.8);
+    }
+
+    #[test]
+    fn test_checkout_with_multiple_items() {
+        assert_total(vec![Apple, Tomato, Mango, Tomato, Apple], 3.1)
     }
 }
--- rustcast/episodes/0002-tdd-checkout/src/price_list.rs
+++ rustcast/episodes/0002-tdd-checkout/src/price_list.rs
@@ -33,3 +33,27 @@ impl PriceList {
         }
     }
 }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_price_list_default() {
+        let price_list = PriceList::new();
+        assert_eq!(price_list.get(Apple), 0.0);
+        assert_eq!(price_list.get(Tomato), 0.0);
+        assert_eq!(price_list.get(Mango), 0.0);
+    }
+
+    #[test]
+    fn test_price_list_set_and_get_prices() {
+        let mut price_list = PriceList::new();
+        price_list.set(Apple, 0.40);
+        price_list.set(Tomato, 0.25);
+        price_list.set(Mango, 1.80);
+
+        assert_eq!(price_list.get(Apple), 0.40);
+        assert_eq!(price_list.get(Tomato), 0.25);
+        assert_eq!(price_list.get(Mango), 1.80);
+    }
+}
