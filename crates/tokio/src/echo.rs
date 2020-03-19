232196cf4cb67de43884588b38f7e501c49ee7c3
utx0 <utx0@protonmail.com>
2020-03-19 20:28:20 +1100

    Got tokio echo server working.

--- crates/tokio/src/echo.rs
+++ crates/tokio/src/echo.rs
@@ -1,43 +1,34 @@
-use futures::stream::StreamExt;
 use tokio::net::TcpListener;
 use tokio::prelude::*;
 
 #[tokio::main]
-pub async fn main() {
-    let addr = "127.0.0.1:6142";
-    let mut listener = TcpListener::bind(addr).await.unwrap();
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    let server = async move {
-        let mut incoming = listener.incoming();
-        while let Some(conn) = incoming.next().await {
-            match conn {
-                Ok(mut socket) => {
-                    println!("Accepted connection from {:?}", socket.peer_addr());
+    loop {
+        let (mut socket, _) = listener.accept().await?;
 
-                    tokio::spawn(async move {
-                        let (mut reader, mut writer) = socket.split();
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
 
-                        println!("{:?}", writer);
-                        println!("{:?}", reader);
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
 
-                        match tokio::io::copy(&mut reader, &mut writer).await {
-                            Ok(amt) => {
-                                eprintln!("wrote {} bytes", amt);
-                            }
-                            Err(err) => {
-                                eprintln!("IO error {:?}", err);
-                            }
-                        }
-                    });
-                }
-                Err(err) => {
-                    eprintln!("Accept error = {:?}", err);
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
                 }
             }
-        }
-    };
-
-    println!("Server running on localhost:6142");
-
-    server.await;
+        });
+    }
 }
--- crates/tokio/src/main.rs
+++ crates/tokio/src/main.rs
@@ -1,6 +1,5 @@
 // mod helloworld;
 mod echo;
-
 fn main() {
     // helloworld::hello();
     echo::main();
